0693b3352eb051877ed378c804104b66df7e0d51
Anurag Anand <anuraganand789@hotmail.com>
2021-02-27 22:22:39 +0530

    code for creating a rest web service

---
+++ rest-service/src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,15 @@
+package com.example.restservice;
+
+public class Greeting{
+    private final long id;
+    private final String message;
+
+    public Greeting(final long id, final String message){
+        this.id = id;
+	this.message = message;
+    }
+
+    public long getId() { return this.id; }
+    public String getMessage() { return this.message; }
+
+}
